6a8e8ed1e9c7e8572419f6a89e43bd3595d3bce3
Azamat <azamat.abdullin@ringcentral.com>
2021-12-19 22:32:18 +0300

    SubQuery HeroCourse: Module 3, exercise #1

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,24 @@
+import {SubstrateEvent} from "@subql/types";
+import {Account, Transfer} from "../types";
+import {Balance} from "@polkadot/types/interfaces";
+
+export async function handleEvent(event: SubstrateEvent): Promise<void> {
+    const [toAddress, amount] = event.event.data;
+
+    console.log('toAddress: ' + toAddress);
+    console.log('amount: ' + amount);
+    console.log('typeof amount - "' + typeof amount + '"');
+
+    const toAccount = await Account.get(toAddress.toString());
+    if (!toAccount) {
+        await new Account(toAddress.toString()).save();
+    }
+
+    const transfer = new
+        Transfer(`${event.block.block.header.number.toNumber()}-${event.idx}`,
+    );
+    transfer.blockNumber = event.block.block.header.number.toBigInt();
+    transfer.toId = toAddress.toString();
+    transfer.amount = (amount as Balance).toBigInt();
+    await transfer.save();
+}
